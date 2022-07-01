79cb5fabacdba5119a997030c9a05e82fe5ebb11
DewiPutriS <dewiputrikaneki@gmail.com>
2022-07-01 22:37:31 +0700

    menambah controller

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    res.status(200).json({message: 'Get Goals'})
-})
+const {getGoals} = require('../controllers/goalController')
+
+router.get('/', getGoals)
+
 router.post('/',(req,res)=>{
     res.status(200).json({message: 'Set Goal'})
 })
