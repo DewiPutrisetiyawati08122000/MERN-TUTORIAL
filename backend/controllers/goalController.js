82c6b3a882a929eb19781331afcde9147539ef07
DewiPutrisetiyawati08122000 <191240000887@unisnu.ac.id>
2022-07-14 21:31:35 +0700

    goal controllerjs

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,6 +2,21 @@ const getGoals = (req,res) => {
     res.status(200).json({message: 'Get Goals'})
 }
 
+const setGoal = (req,res) => {
+    res.status(200).json({message: 'Set Goal'})
+}
+
+const updateGoal = (req,res) => {
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
+}
+
+const deleteGoal = (req,res) => {
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+}
+
 module.exports = {
-    GetGoals
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
 }
