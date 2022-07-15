19e4c58f876bf8454cf43311751e93368120a2cd
DewiPutrisetiyawati08122000 <191240000887@unisnu.ac.id>
2022-07-15 21:05:42 +0700

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,31 @@
-const registerUser = (req, res) => {
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+    if (!name || !email || !password){
+        res.status(400)
+        throw Error('Please add all fields')
+    }
+
+    const userExist = await User.findOne({email})
+
+    if(userExist){
+        res.status(400)
+        throw Error('User already exists')
+    }
     res.json({message: "Register User"})
-}
+})
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.json({message: "Login User"})
-}
+})
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.json({message: "User data display"})
-}
+})
 
 module.exports = {
     registerUser,
