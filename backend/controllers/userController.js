6271d40218e7cd55f08294376a4c3df559f32ac4
DewiPutrisetiyawati08122000 <191240000887@unisnu.ac.id>
2022-07-15 21:19:29 +0700

    authenticate

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,11 +36,23 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw Error('invalid user data')
     }
-
-    res.json({message: "Register User"})
 })
 
 const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({email})
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw Error('invalid credentials')
+    }
+
     res.json({message: "Login User"})
 })
 
