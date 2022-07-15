fd11f1e7c91deed98077d1f3f8c9f692d1da0aca
DewiPutrisetiyawati08122000 <191240000887@unisnu.ac.id>
2022-07-15 21:26:12 +0700

    Menambahkan JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,6 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token : generateToken(user._id),
         })
     }else {
         res.status(400)
@@ -47,6 +48,7 @@ const loginUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token : generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -59,6 +61,12 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
     res.json({message: "User data display"})
 })
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+};
 
 module.exports = {
     registerUser,
