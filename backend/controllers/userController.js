65a0a45f392aba83cb860704396e70cf93dfe327
DewiPutrisetiyawati08122000 <191240000887@unisnu.ac.id>
2022-07-15 21:11:48 +0700

    hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,6 +16,27 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw Error('User already exists')
     }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    }else {
+        res.status(400)
+        throw Error('invalid user data')
+    }
+
     res.json({message: "Register User"})
 })
 
